aebfba3e59e11a2d30cb93bc5f176a3ce829dea7
Toni Kazinoti <tkazinoti@hrcloud.corp>
2021-07-16 16:17:04 +0200

    first commit

---
+++ services.js
@@ -0,0 +1,26 @@
+// SERVICES
+weatherApp.service('cityService', function () {
+  this.city = 'New York';
+});
+
+weatherApp.service('weatherService', [
+  '$http',
+  function ($http) {
+    this.GetWeather = async function (city, days) {
+      var res = null;
+
+      res = await $http({
+        method: 'GET',
+        url: `https://community-open-weather-map.p.rapidapi.com/find?q=${city}&cnt=${days}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'x-rapidapi-key':
+            '44e6fe78bemsh5949731699abf94p1fc826jsn4520083dfca0',
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+      // console.log(res.data);
+      return res.data;
+    };
+  },
+]);
